1d6b86f636db39c1358734795d2b6a5991bf942e
seemsartless <david.s.toronto@gmail.com>
2024-05-30 22:29:32 -0400

    V3.2 - additional columns added, and filenames updated

--- experiment/task-01-data-collection/David-Sky/one_tweet_responses.py
+++ experiment/task-01-data-collection/David-Sky/one_tweet_responses.py
@@ -7,12 +7,16 @@ import xlsxwriter
 import getpass
 import time
 
-# Given a single tweet URL like
+# Retrieve all responses from a given a single tweet URL like:
 #    https://twitter.com/MaxKevinC/status/1781174881050128396
 #
-# Retrieve all the responses
-#
-script_v = "V3.1"
+# Or process all the URLs, one per line, in a text file specified, like:
+#     /Users/davidsky/PycharmProjectselsalvador-local/tweet_list_to_process.txt
+
+script_v = "V3.2"
+# V3.2 - multiple additions
+#          - add author id and author name of the political actor that posting the post
+#          - add version number to filename
 # V3.1 - add location and when the user started using twitter
 # V3.0 - support a text file with a list of tweets to process
 # V2.1 - bit more code cleanup - still expects a single Tweet to process, no list support yet
@@ -64,11 +68,27 @@ def tweet_to_df(df, tweet_dict):
             # And author_createdAt
             sub_value2 = value['createdAt']
             parsed_date = datetime.strptime(sub_value2, '%a %b %d %H:%M:%S %z %Y')
-
             # Format the datetime object into an ISO formatted date string
             iso_formatted_date = parsed_date.strftime('%Y-%m-%d')
-
             df.loc[new_row, 'author__createdAt'] = iso_formatted_date
+
+            # Some items JUST for the political actors - V3.2
+            sub_valueAI = value['id']
+            df.loc[new_row, 'author__id'] = sub_valueAI
+
+            sub_valueAN = value['name']
+            df.loc[new_row, 'author__name'] = sub_valueAN
+
+            sub_valueAD = value['description']
+            df.loc[new_row, 'author__description'] = sub_valueAD
+
+            sub_valueAF = value['followers']
+            df.loc[new_row, 'author__followers'] = sub_valueAF
+
+            sub_valueAV = value['isVerified']
+            df.loc[new_row, 'author__verified'] = sub_valueAV
+
+
 
         else:
             # And the rest of the top level values
@@ -127,10 +147,32 @@ def main():
         "lang",
         "author__createdAt",
         "author__location",
+        # Added for V3.2 for JUST the political actor collections
+        "author__name",
+        "author__id",
+        "author__description",
+        "author__followers",
+        "author__verified",
         "text"
+    ]
+    remove_for_comments_column = [  # New for V3.2
+        "author__name",
+        "author__id",
+        "author__description",
+        "author__followers",
+        "author__verified",
     ]
 
 
+    log_columns = [  # V3.2 - write a log file for each time the script is run
+        "initial_url",
+        "response_count",
+        "script_duration_secs",
+        "generated_date_utc",
+        "generated_by",
+        "script_name",
+        "script_version",
+    ]
 
     # Get the Apify token and connect
     if your_api_token is not None:
@@ -172,12 +214,14 @@ def main():
         for one_test in to_process_list:
             print(f"\t- {one_test}")
 
+    # V3.2
+    log_df = pd.DataFrame(columns=log_columns)
     # Now run through all the values in to_process_list
     for one_tweet_url in to_process_list:
         print(f"Process the single URL {one_tweet_url}")
         one_tweet_id = str(one_tweet_url.split('/')[-1])
         one_tweet_user = one_tweet_url.split('/')[-3]
-        one_tweet_string = f"{one_tweet_user}_X_replies_{one_tweet_id}"
+        one_tweet_string = f"{one_tweet_user}_X_{script_v}_replies_{one_tweet_id}"  # V3.2 added script_v to filename
         print(f"Will process the tweet: {one_tweet_string}")
 
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -199,6 +243,12 @@ def main():
         df_conv = pd.DataFrame(columns=column_list)
         df_conv_tweets = call_apify_client(reason_text="response tweets", apify_client=client, tweet_url=one_tweet_id, actor_id=apify_actor_id, actor_input=run_input_conv, df=df_conv)
 
+        # V3.2 - drop some of the column names we DON'T want to collect for the people commenting
+        try:
+            df_conv_tweets.drop(columns=remove_for_comments_column, inplace=True)
+        except:
+            print(f"Could not find: {remove_for_comments_column}")
+            print(df_conv_tweets.columns)
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         # Final step, create the Excel file with multiple tabs
         local_excel = f"{local_excel_path}/{one_tweet_string}.xlsx"
@@ -218,8 +268,27 @@ def main():
 
         print(f"\nCreated Excel file:\n\t{local_excel}")
         # End of processing one row in the list
+        # V3.2 - add the details for this specific tweet to the log_df.
+        new_log_row = {
+            "initial_url": one_tweet_url,
+            "response_count": len(df_conv),
+            "script_duration_secs": elapsed_time,
+            "generated_date_utc": datetime.utcnow(),
+            "generated_by": generated_by,
+            "script_name": python_code_name,
+            "script_version": script_v,
+            "output_file_name": f"{one_tweet_string}.xlsx"
+        }
+        new_row_df = pd.DataFrame([new_log_row])
+        log_df = pd.concat([log_df, new_row_df], ignore_index=True)
 
+    # Done processing one or more items, now write the log file
+    log_file_name = datetime.utcnow().strftime('%Y-%m-%d_at_%H_%M_%S')
+    log_file_full = f"{local_excel_path}/{log_file_name}.csv"
+    print(f"Done! Writing log to {log_file_name}")
+    log_df.to_csv(log_file_full, index=False)
     # End of main()
 
 if __name__ == "__main__":
+
     main()
